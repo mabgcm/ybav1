e8aa8138ff11873458b59b349e1ecce67e3b44ff
mabgcm <bugucam@gmail.com>
2022-12-27 01:38:43 +0700

    add project placeholders removed

--- src/pages/AddEditBlog.js
+++ src/pages/AddEditBlog.js
@@ -216,146 +216,146 @@ const AddEditBlog = ({ user, setActive }) => {
               <div className="d-d-md-block none">
                 <div className="content">
                   <div className="box1">
-                    <div className="box1a"><input className='w-100 h-100' placeholder="1a-->" name="box1a"
+                    <div className="box1a"><input className='w-100 h-100' placeholder="" name="box1a"
                       value={box1a}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box1b"><input className='w-100 h-100' placeholder="1b-->" name="box1b"
+                    <div className="box1b"><input className='w-100 h-100' placeholder="" name="box1b"
                       value={box1b}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box1c"><input className='w-100 h-100' placeholder="1c-->" name="box1c"
+                    <div className="box1c"><input className='w-100 h-100' placeholder="" name="box1c"
                       value={box1c}
                       onChange={handleChange} type="text" /></div>
                   </div>
-                  <div className="box2"><input className='w-100 h-100' placeholder="2-->" type="text" name="box2"
+                  <div className="box2"><input className='w-100 h-100' placeholder="" type="text" name="box2"
                     value={box2}
                     onChange={handleChange} /></div>
                   <div className="box3">
-                    <div className="box3a"><input className='w-100 h-100' placeholder="3a-->" name="box3a"
+                    <div className="box3a"><input className='w-100 h-100' placeholder="" name="box3a"
                       value={box3a}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box3b"><input className='w-100 h-100' placeholder="3b-->" name="box3b"
+                    <div className="box3b"><input className='w-100 h-100' placeholder="" name="box3b"
                       value={box3b}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box3c"><input className='w-100 h-100' placeholder="3c-->" name="box3c"
+                    <div className="box3c"><input className='w-100 h-100' placeholder="" name="box3c"
                       value={box3c}
                       onChange={handleChange} type="text" /></div>
                   </div>
-                  <div className="box4"><input className='w-100 h-100' placeholder="4-->" name="box4"
+                  <div className="box4"><input className='w-100 h-100' placeholder="" name="box4"
                     value={box4}
                     onChange={handleChange} type="text" /></div>
                   <div className="box5">
-                    <div className="box5a"><input className='w-100 h-100' placeholder="5a-->" name="box5a"
+                    <div className="box5a"><input className='w-100 h-100' placeholder="" name="box5a"
                       value={box5a}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box5b"><input className='w-100 h-100' placeholder="5b-->" name="box5b"
+                    <div className="box5b"><input className='w-100 h-100' placeholder="" name="box5b"
                       value={box5b}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box5c"><input className='w-100 h-100' placeholder="5c-->" name="box5c"
+                    <div className="box5c"><input className='w-100 h-100' placeholder="" name="box5c"
                       value={box5c}
                       onChange={handleChange} type="text" /></div>
                   </div>
                   <div className="box6">
-                    <div className="box6a"><input className='w-100 h-100' placeholder="6a-->" name="box6a"
+                    <div className="box6a"><input className='w-100 h-100' placeholder="" name="box6a"
                       value={box6a}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box6b"><input className='w-100 h-100' placeholder="6b-->" name="box6b"
+                    <div className="box6b"><input className='w-100 h-100' placeholder="" name="box6b"
                       value={box6b}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box6c"><input className='w-100 h-100' placeholder="6c-->" name="box6c"
+                    <div className="box6c"><input className='w-100 h-100' placeholder="" name="box6c"
                       value={box6c}
                       onChange={handleChange} type="text" /></div>
                   </div>
-                  <div className="box7"><input placeholder="7-->" name="box7"
+                  <div className="box7"><input placeholder="" name="box7"
                     value={box7}
                     onChange={handleChange} type="text" /></div>
-                  <div className="box8"><input placeholder="8-->" name="box8"
+                  <div className="box8"><input placeholder="" name="box8"
                     value={box8}
                     onChange={handleChange} type="text" /></div>
-                  <div className="box9"><input placeholder="9-->" name="box9"
+                  <div className="box9"><input placeholder="" name="box9"
                     value={box9}
                     onChange={handleChange} type="text" /></div>
-                  <div className="box10"><input placeholder="10-->" name="box10"
+                  <div className="box10"><input placeholder="" name="box10"
                     value={box10}
                     onChange={handleChange} type="text" /></div>
                   <div className="box11">
-                    <div className="box11a"><input className='w-100 h-100' placeholder="11a-->" name="box11a"
+                    <div className="box11a"><input className='w-100 h-100' placeholder="" name="box11a"
                       value={box11a}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box11b"><input className='w-100 h-100' placeholder="11b-->" name="box11b"
+                    <div className="box11b"><input className='w-100 h-100' placeholder="" name="box11b"
                       value={box11b}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box11c"><input className='w-100 h-100' placeholder="11c-->" name="box11c"
+                    <div className="box11c"><input className='w-100 h-100' placeholder="" name="box11c"
                       value={box11c}
                       onChange={handleChange} type="text" /></div>
                   </div>
                   <div className="box12">
-                    <div className="box12a"><input className='w-100 h-100' placeholder="12a-->" name="box12a"
+                    <div className="box12a"><input className='w-100 h-100' placeholder="" name="box12a"
                       value={box12a}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box12b"><input className='w-100 h-100' placeholder="12b-->" name="box12b"
+                    <div className="box12b"><input className='w-100 h-100' placeholder="" name="box12b"
                       value={box12b}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box12c"><input className='w-100 h-100' placeholder="12c-->" name="box12c"
+                    <div className="box12c"><input className='w-100 h-100' placeholder="" name="box12c"
                       value={box12c}
                       onChange={handleChange} type="text" /></div>
                   </div>
-                  <div className="box13"><input className='w-100 h-100' placeholder="13-->" name="box13"
+                  <div className="box13"><input className='w-100 h-100' placeholder="" name="box13"
                     value={box13}
                     onChange={handleChange} type="text" /></div>
                   <div className="box14">
-                    <div className="box14a"><input className='w-100 h-100' placeholder="14a-->" name="box14a"
+                    <div className="box14a"><input className='w-100 h-100' placeholder="" name="box14a"
                       value={box14a}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box14b"><input className='w-100 h-100' placeholder="14b-->" name="box14b"
+                    <div className="box14b"><input className='w-100 h-100' placeholder="" name="box14b"
                       value={box14b}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box14c"><input className='w-100 h-100' placeholder="14c-->" name="box14c"
+                    <div className="box14c"><input className='w-100 h-100' placeholder="" name="box14c"
                       value={box14c}
                       onChange={handleChange} type="text" /></div>
                   </div>
                   <div className="box15">
-                    <div className="box15a"><input className='w-100 h-100' placeholder="15a-->" name="box15a"
+                    <div className="box15a"><input className='w-100 h-100' placeholder="" name="box15a"
                       value={box15a}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box15b"><input className='w-100 h-100' placeholder="15b-->" name="box15b"
+                    <div className="box15b"><input className='w-100 h-100' placeholder="" name="box15b"
                       value={box15b}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box15c"><input className='w-100 h-100' placeholder="15c-->" name="box15c"
+                    <div className="box15c"><input className='w-100 h-100' placeholder="" name="box15c"
                       value={box15c}
                       onChange={handleChange} type="text" /></div>
                   </div>
-                  <div className="box16"><input className='w-100 h-100' placeholder="16-->" name="box16"
+                  <div className="box16"><input className='w-100 h-100' placeholder="" name="box16"
                     value={box16}
                     onChange={handleChange} type="text" /></div>
                   <div className="box17">
-                    <div className="box17a"><input placeholder="17a-->" className='w-100 h-100' name="box17a"
+                    <div className="box17a"><input placeholder="" className='w-100 h-100' name="box17a"
                       value={box17a}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box17b"><input placeholder="17b-->" className='w-100 h-100' name="box17b"
+                    <div className="box17b"><input placeholder="" className='w-100 h-100' name="box17b"
                       value={box17b}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box17c"><input placeholder="17c-->" className='w-100 h-100' name="box17c"
+                    <div className="box17c"><input placeholder="" className='w-100 h-100' name="box17c"
                       value={box17c}
                       onChange={handleChange} type="text" /></div>
                     <div className="box17d"></div>
-                    <div className="box17e"><input placeholder="17e-->" className='w-100 h-100' name="box17e"
+                    <div className="box17e"><input placeholder="" className='w-100 h-100' name="box17e"
                       value={box17e}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box17f"><input placeholder="17f-->" className='w-100 h-100' name="box17f"
+                    <div className="box17f"><input placeholder="" className='w-100 h-100' name="box17f"
                       value={box17f}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box17g"><input placeholder="17g-->" className='w-100 h-100' name="box17g"
+                    <div className="box17g"><input placeholder="" className='w-100 h-100' name="box17g"
                       value={box17g}
                       onChange={handleChange} type="text" /></div>
                   </div>
                   <div className="box18">
-                    <div className="box18a"><input placeholder="18a-->" className='w-100 h-100' name="box18a"
+                    <div className="box18a"><input placeholder="" className='w-100 h-100' name="box18a"
                       value={box18a}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box18b"><input placeholder="18b-->" className='w-100 h-100' name="box18b"
+                    <div className="box18b"><input placeholder="" className='w-100 h-100' name="box18b"
                       value={box18b}
                       onChange={handleChange} type="text" /></div>
-                    <div className="box18c"><input placeholder="18c-->" className='w-100 h-100' name="box18c"
+                    <div className="box18c"><input placeholder="" className='w-100 h-100' name="box18c"
                       value={box18c}
                       onChange={handleChange} type="text" />
                     </div>
